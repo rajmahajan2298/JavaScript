52d480a4aa2402ad86210726c02f021be46c0c3e
Raj Mahajan <programming2298@gmail.com>
2024-09-18 17:39:11 +0530

    Different Functions

---
+++ more.js
@@ -0,0 +1,23 @@
+// Function Statement
+
+function x() {
+    console.log("x called");
+}
+
+// Function Expersion
+
+var y = function() {
+    console.log("y called");
+}
+
+// The Major difference b/w Statement nd Declarartions is Hositing
+
+// Anaymous Function
+// Function Without Name Is Anaymous Function which don't have there own identity.
+// It is used when they are used as value
+
+// Named Function
+
+var y = function xyz() {
+    console.log("y called")
+}
